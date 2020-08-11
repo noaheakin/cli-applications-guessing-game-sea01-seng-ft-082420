29c66b899f49859842b3027e77361ad9d203b9fe
Noah Eakin <noaheakin@gmail.com>
2020-08-11 23:22:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   number = rand(1..6).to_s
   guess = gets.chomp.to_s
-  if guess === number
+  if guess == number
     puts "You guessed the correct number!"
   elsif guess.to_s == 'exit'
     puts "Goodbye!"
