9de67994f757bbb6c8c4d2eaed8fc9d3c7841e55
Noah Eakin <noaheakin@gmail.com>
2020-08-11 21:57:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -13,7 +13,7 @@ end
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
-  comp_num = rand (1..6)
+  comp_num = rand(1..6)
   user_input = gets.chomp
   if user_input == comp_num
     puts "You guessed the correct number!"
