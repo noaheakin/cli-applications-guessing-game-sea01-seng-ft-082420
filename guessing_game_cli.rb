6ac82d8ec26311e9e546ec222ece0d7c8e847d60
Noah Eakin <noaheakin@gmail.com>
2020-08-11 20:21:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,11 @@
-# Code your solution here!
+def random_number_generator
+  rand 1..6
+end
+
+def prompt_user
+  puts "Guess a number between 1 and 6"
+end
+
+def run_guessing_game
+  comp_num = random_number_generator
+end
