2c305a29dae73b91548ee709e2e3289ea9831663
Noah Eakin <noaheakin@gmail.com>
2020-08-11 20:49:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,11 +10,11 @@ end
 def run_guessing_game
   comp_num = random_number_generator
   user_input = gets.chomp
-  if user_input == 'exit'
-    puts "Goodbye!"
-  elsif user_input == comp_num
+  if user_input == comp_num
     puts "You guessed the correct number!"
-  else
+  elsif user_input == 'exit'
+    puts "Goodbye!"
+  elsif user_input != 'comp_num'
     puts "Sorry! The computer guessed #{comp_num}."
   end
 end
